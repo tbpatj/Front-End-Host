61903f15f298da3171e85484bbabeb8fbef2f8b8
Timothy Luebke <luebketj@gmail.com>
2024-04-15 16:48:57 -0600

    feat: added neccisary files and code to host a front end server

---
+++ index.js
@@ -0,0 +1,45 @@
+//load up the required modules
+const express = require("express");
+const cors = require("cors");
+const { shipAscii } = require("./ship");
+require("dotenv").config();
+
+//load up the env variables
+const { PORT, CORS_ALLOWED_URLS } = process.env;
+
+//start up the app
+const app = express();
+
+//enable cors
+try {
+  const allowedUrls = CORS_ALLOWED_URLS.split(",");
+  app.use(cors({ origin: allowedUrls }));
+} catch (e) {
+  console.error("\nPlease create a .env that includes: CORS_ALLOWED_URLS");
+  console.log(
+    "CORS_ALLOWED_URLS should be a comma separated list of urls that are allowed to access this server.\n"
+  );
+}
+
+//use json
+app.use(express.json());
+
+//serve all the neccesary files when we hit the main endpoint
+app.use("/", (req, res) => {
+  res.sendFile(osFilePath("build/index.html"), { root: __dirname });
+});
+
+//start the server if the port has been provided in a .env
+if (!PORT) {
+  console.error("\nPlease create a .env that includes: PORT");
+  console.log(
+    "PORT should be the port number that the server should listen on.\n"
+  );
+  process.exit(1);
+}
+app.listen(PORT, () => {
+  console.log(
+    shipAscii,
+    `\nAye ship set sail from port: ${PORT}, swab the deck and keep a lookout for the front-end that has been hoisted.`
+  );
+});
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Usage
+
+This is a quick and simple node server that is meant to just serve up a simple built front-end that is all housed within a single build folder in the same directory
+
+# .ENV
+
+make sure to create a .env file with the following properties
+
+```
+PORT=_THE PORT THAT THE SERVER WILL RUN ON_
+CORS_ALLOWED_URLS=_THE ALLOWED URLS TO SERVER TO_
+```
+
+CORS_ALLOWED_URLS should be a comma separated list of urls that are allowed to access this server.
